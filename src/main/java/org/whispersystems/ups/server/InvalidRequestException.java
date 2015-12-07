b83aa2cb7eef8c257f2e3f3e0edf16e253acc3cb
Jani Monoses <jani@ubuntu.com>
2015-12-07 11:13:42 +0200

    Rename package for exception classes.

--- src/main/java/org/whispersystems/ups/server/InvalidRequestException.java
+++ src/main/java/org/whispersystems/ups/server/InvalidRequestException.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.whispersystems.gcm.server;
+package org.whispersystems.ups.server;
 
 
 public class InvalidRequestException extends Exception {
--- src/main/java/org/whispersystems/ups/server/ServerFailedException.java
+++ src/main/java/org/whispersystems/ups/server/ServerFailedException.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.whispersystems.gcm.server;
+package org.whispersystems.ups.server;
 
 public class ServerFailedException extends Exception {
   public ServerFailedException(String message) {
